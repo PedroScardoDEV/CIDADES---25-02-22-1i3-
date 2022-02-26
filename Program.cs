e5c85f62f69f717e80de098f815a1c73cd6016ff
Pedro <pedroscardovelli@gmail.com>
2022-02-25 22:32:11 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ubatuba");
+Console.WriteLine("Caraguatatuba");
+Console.WriteLine("Ilhabela");
+Console.WriteLine("Sao Sebastiao");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Guaruja");
+Console.WriteLine("Santos");
+Console.WriteLine("Sao Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongagua");
+Console.WriteLine("Itanhaem");
+Console.WriteLine("Peruibe");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Cananeia");
+Console.WriteLine("Iguape");
